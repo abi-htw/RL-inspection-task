f584a2d696268667096bba04e9a38e03e80f8c1b
Abishek Sunilkumar <dsara918@informatik.htw-dresden.de>
2023-08-30 16:31:39 +0000

    Add table

--- omniisaacgymenvs/robots/articulations/Box.py
+++ omniisaacgymenvs/robots/articulations/Box.py
@@ -20,6 +20,7 @@ class Box(Robot):
         usd_path: Optional[str] = None,
         translation: Optional[torch.tensor] = None,
         orientation: Optional[torch.tensor] = None,
+        scale: Optional[torch.tensor] = None,
     ) -> None:
         """[summary]
         """
@@ -30,13 +31,15 @@ class Box(Robot):
             assets_root_path = get_assets_root_path()
             if assets_root_path is None:
                 carb.log_error("Could not find Isaac Sim assets folder")
-            self._usd_path = "/inst_assets/Isaac/2022.1/Isaac/Props/Box/box_instantiable.usd"
+            self._usd_path = "/inst_assets//table_usd/table_4.usd"
             # self._usd_path = "/inst_assets/Isaac/2022.1/Isaac/Props/Box/small_KLT.usd"
 
 
 
-        self._position = torch.tensor([1.1, 0.8, 0.1]) if translation is None else translation
+        self._position = torch.tensor([0.0, 0.0, 0.0]) if translation is None else translation
         self._orientation = torch.tensor([0.1, 0.0, 0.0, 0.0]) if orientation is None else orientation
+        self._scale = torch.tensor([0.01, 0.01, 0.01]) if scale is None else scale  # Default scale is [1, 1, 1]
+
 
         add_reference_to_stage(self._usd_path, prim_path)
 
@@ -45,5 +48,6 @@ class Box(Robot):
             name=name,
             translation=self._position,
             orientation=self._orientation,
+            scale=self._scale,
             articulation_controller=None,
         )
